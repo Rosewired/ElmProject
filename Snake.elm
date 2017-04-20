e24570c10387ce607c6fe87b44005d053860cfe7
Rosewired <branaugh@Branaughs-MacBook-Pro.local>
2017-04-20 08:22:29 -0600

    4/20 8:22 AM

--- Snake.elm
+++ Snake.elm
@@ -1,8 +1,11 @@
+-- Rosewired@mac.com
+module Snake exposing (..)
 -- the Elm architecture
 import Html
 -- html style and helper functions for HTML attributes
 --import Html.App
--- access of direction and key codes
+-- access of direction and key codes,
+-- let application listen keyboard event
 import Keyboard
 -- allow us to create a grid to run the game
 import Window
@@ -15,13 +18,15 @@ import Html.Attributes exposing (style)
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
 
+(width, height) = (500, 500)
+
 main =
-  App.program
-  { init = init
-  , view = view
-  , update = update
-  , subscriptions = subscriptions
-  }
+  program
+    { init = init
+    , view = view
+    , update = update
+    , subscriptions = subscriptions
+    }
 
 -- model
 type alias Game =
@@ -32,21 +37,14 @@ type alias Game =
   , score : Int
   }
 
-type alias GameState = NewGame
-                     | InGame
-                     | Lose
+type GameState = NewGame
+               | InGame
+               | Lose
 
 type Direction = Up
                | Down
                | Left
                | Right
-
--- key presses
-type Keys = NoKey
-          | UpKey
-          | DownKey
-          | LeftKey
-          | RightKey
 
 type alias Block = (Float, Float)
 
@@ -66,13 +64,60 @@ initSnake =
   , direction = Left
   }
 
-init : Game
-init =
+initGame =
   { snake = initSnake
   , state = NewGame
   , food = Nothing
-  , eaten : False
-  , score : Int
+  , eaten = False
+  , score = Int
   }
 
--- define subscriptions
+init : (Game, Cmd Msg)
+init = (initGame, Cmd.none)
+
+
+-- MESSAGES
+type Msg
+  = KeyMsg Keyboard.KeyCode
+
+-- UPDATE
+update : Msg -> Game -> (Game, Cmd Msg)
+update msg ({snake, state, food, eaten, score} as game) =
+  case state of
+    NewGame ->
+      case msg of
+        -- space
+        KeyPress 32 ->
+          ({initSnake, InGame, Nothing, False, 0}, Cmd.none)
+
+        _ ->
+          (game, Cmd.none)
+    InGame ->
+      ({initSnake, InGame, Nothing, False, 0}, Cmd.none)
+
+    Lose ->
+      ({initSnake, NewGame, Nothing, False, 0}, Cmd.none)
+
+-- VIEW
+view : Game -> Html Msg
+view game =
+  let background = rect (toFloat width) (toFloat height) |> filled blue
+  content =
+    case game.state of
+      NewGame ->
+        [txt "press SPACE to start"]
+      InGame ->
+        [txt "You already enter the game !!!"]
+
+
+-- SUBSCRIPTIONS
+subscriptions : Game -> Sub Msg
+subscriptions  game =
+  case game.state of
+    InGame ->
+      Keyboard.presses KeyPress
+    _ ->
+      Sub.batch
+        [ Keyboard.downs KeyMsg
+        , Time.every (Time.inMilliseconds 50) Tick
+        ]
